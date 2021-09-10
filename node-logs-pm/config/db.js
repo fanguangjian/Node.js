e1f1640ca1edd88fcc53c29826df513c962437d1
G.F <fanguangjianauto@163.com>
2021-09-10 21:49:17 +1000

    add fix

--- node-logs-pm/config/db.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-08-26 13:40:20
- * @LastEditTime: 2021-08-26 13:40:20
- * @LastEditors: NEWAIMLAP-227
- * @Description: In User Settings Edit
- * @FilePath: \Invoice-service-node\conf\db.js
- */
